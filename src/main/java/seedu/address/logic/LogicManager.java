76d2aa9368854cb62955bfb25fc3e9c73b889bfa
kimshitong <shitongkim28@gmail.com>
2023-10-11 10:15:50 +0800

    Create List for Doctor in Logic

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -16,6 +16,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.doctor.Doctor;
 import seedu.address.storage.Storage;
 
 /**
@@ -69,6 +70,10 @@ public class LogicManager implements Logic {
     @Override
     public ObservableList<Person> getFilteredPersonList() {
         return model.getFilteredPersonList();
+    }
+    @Override
+    public ObservableList<Doctor> getFilteredDoctorList() {
+        return model.getFilteredDoctorList();
     }
 
     @Override
